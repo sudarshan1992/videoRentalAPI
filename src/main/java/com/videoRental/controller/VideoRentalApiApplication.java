500b9f866a3796825610c5931c35ee202fa01290
HAPPIESTMINDS\Sudarshan.selvam <sudarshan.selvam@happiestminds.com>
2017-06-13 13:24:25 +0530

    initail commit

---
+++ src/main/java/com/videoRental/controller/VideoRentalApiApplication.java
@@ -0,0 +1,12 @@
+package com.videoRental.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalApiApplication.class, args);
+	}
+}
